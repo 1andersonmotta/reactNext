db4da6febc1d0f1ef56fa51175672f85db65759c
1andersonmotta <devandersonmotta@gmail.com>
2023-12-06 18:11:39 -0300

    adjust in index for component video background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@
 // import { Inter } from 'next/font/google'
 // const inter = Inter({ subsets: ['latin'] })
 
+import VideoComponent from "@/component/background-video"
+
 const nome = "Anderson"
 let idade = "38"
 
@@ -16,10 +18,11 @@ function Topo() {
 
 export default function Home() {
   return (
-    <main className="flex justify-around items-center bg-slate-500 h-[100px]">
-      <Topo></Topo>
+    <main className="flex justify-around items-center  h-[100px]">
+      {/* <Topo></Topo>
       <div >olá mundo do {nome}</div>
-      <div>olá mundo - {idade}</div>
+      <div>olá mundo - {idade}</div> */}
+      <VideoComponent></VideoComponent>
     </main>
   )
 }
