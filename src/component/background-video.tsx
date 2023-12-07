ba487ad67a106345d806c83c8c52248d109d31e3
1andersonmotta <devandersonmotta@gmail.com>
2023-12-07 18:18:24 -0300

    fix background color

--- src/component/background-video.tsx
+++ src/component/background-video.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const VideoComponent = () => {
     return (
-        <video id="video-background" autoPlay muted loop>
-            <source src="images/video.mp4" type="video/mp4" />
+        <video >
+            <source src="./images/video.mp4" type="video/mp4" />
         </video>
     );
 };
