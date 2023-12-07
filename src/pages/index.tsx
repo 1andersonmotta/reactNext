0bb01a6a3222ff0712737229ec1aed0e33d56de4
1andersonmotta <devandersonmotta@gmail.com>
2023-12-07 18:18:07 -0300

    clear code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,24 +4,10 @@
 
 import VideoComponent from "@/component/background-video"
 
-const nome = "Anderson"
-let idade = "38"
-
-function Topo() {
-  return (
-    <div>
-      <div>Topo do {nome}</div>
-      <div>Topo da {idade}</div>
-    </div>
-  )
-}
-
 export default function Home() {
   return (
-    <main className="flex justify-around items-center  h-[100px]">
-      {/* <Topo></Topo>
-      <div >olá mundo do {nome}</div>
-      <div>olá mundo - {idade}</div> */}
+    <main className="flex justify-around items-center bg-slate-500 h-[100px]">
+
       <VideoComponent></VideoComponent>
     </main>
   )
