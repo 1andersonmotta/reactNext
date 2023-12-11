bcdc393ea9921801572e5d90caba171a41410b1a
1andersonmotta <devandersonmotta@gmail.com>
2023-12-11 17:47:08 -0300

    feature component index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
+/* eslint-disable react/jsx-no-undef */
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 // const inter = Inter({ subsets: ['latin'] })
 
-import VideoComponent from "@/component/background-video"
+import VideoComponent from "@/component/background-video";
+import Header from "@/component/header";
+import Anime from "@/component/anime";
+import Component from "@/component/component";
+import Main from "@/component/main";
 
 export default function Home() {
   return (
-    <main className="flex justify-around items-center bg-slate-500 h-[100px]">
-
+    <main >
+      <Header />
       <VideoComponent></VideoComponent>
+      <Component></Component>
+      <Main></Main>
+      <Anime></Anime>
     </main>
   )
 }
