ac5a8bc49adf004bd57a65736b32c9e62517bf52
1andersonmotta <devandersonmotta@gmail.com>
2023-12-12 11:15:28 -0300

    feature create Services component

---
+++ src/components/services.tsx
@@ -0,0 +1,68 @@
+const Services = () => {
+    return (
+        <section className="services">
+            <div className="my_services">
+                <ul>
+                    <li>
+                        <i className="fa-sharp fa-solid fa-pen-to-square"></i>
+                        <h3>• Linguagem de Programação </h3>
+                        <p>JavaScript e TypeScript</p>
+
+                    </li>
+                    <li>
+                        <i className="fa-sharp fa-solid fa-pen-to-square"></i>
+                        <h3>• Conhecimentos </h3>
+                        <p>NodeJs, NestJs, Express, TypeORM, Sequelize, Prisma, Serverless
+                            <br />,
+                            Regex, Graphql e React
+                        </p>
+
+                    </li>
+                    <li>
+                        <i className="fa-sharp fa-solid fa-pen-to-square"></i>
+                        <h3>• Linguagem de Marcação</h3>
+                        <p>HTML e CSS </p>
+
+                    </li>
+                    <li>
+                        <i className="fa-sharp fa-solid fa-pen-to-square"></i>
+                        <h3>• Linguagem de Consultas</h3>
+                        <p>SQL</p>
+
+                    </li>
+                    <li>
+                        <i className="fa-sharp fa-solid fa-pen-to-square"></i>
+                        <h3>• Banco de Dados</h3>
+                        <p> Relacional: Postgress, Sqlite3 e MySql.
+                            Não Relacional: MongoDB</p>
+
+                    </li>
+                    <li>
+                        <i className="fa-sharp fa-solid fa-pen-to-square"></i>
+                        <h3>• Arquitetura </h3>
+                        <p>Princípios SOLID, Clean Arquitecture, Monolítico MVC, Arquitetura em
+                            camadas - Hexagonal e Microserviços</p>
+                    </li>
+                    <li>
+                        <i className="fa-sharp fa-solid fa-pen-to-square"></i>
+                        <h3>• Protocolos de Comunicação </h3>
+                        <p>APIs Restful e Protocolo Simples de Acesso a Objetos (SOAP)</p>
+                    </li>
+                    <li>
+                        <i className="fa-sharp fa-solid fa-pen-to-square"></i>
+                        <h3>• Ferramentas </h3>
+                        <p>Visual Studio Code, Docker, Insomnia, Postman, Dbeaver</p>
+                    </li>
+                    <li>
+                        <i className="fa-sharp fa-solid fa-pen-to-square"></i>
+                        <h3>• Bibliotecas</h3>
+                        <p>Jest, Faker, Axios, Supertest, Factory-Girls, SendGrid, Moment, Html-to-Pdf, Puppeter,
+                            Class-Validator, SocketIO, Graphql, Serverless-offline...</p>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    )
+};
+
+export default Services;
