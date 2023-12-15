35837a4060a2d9ca2e54f6c5400688865fe6ee18
1andersonmotta <devandersonmotta@gmail.com>
2023-12-15 11:01:51 -0300

    feature create component facts

---
+++ src/components/facts.jsx
@@ -0,0 +1,79 @@
+const Facts = () => {
+    return (
+        <div className="cursos">
+            <h2><i className="fa-solid fa-heart"></i>FATOS</h2>
+            <div className="funfacts">
+                <ul>
+                    <li>
+                        <i className="fa-solid fa-mug-hot"></i>
+                        <span>100%</span>
+                        <p>Disponível</p>
+                    </li>
+                    <li>
+                        <i className="fa-solid fa-medal"></i>
+                        <span>32</span>
+                        <p>Cerificados</p>
+                    </li>
+                    <li>
+                        <i className="fa-solid fa-graduation-cap"></i>
+                        <span>Disposto</span>
+                        <p>A Estudar </p>
+                    </li>
+                    <li>
+                        <i className="fa-sharp fa-solid fa-hand-holding-dollar"></i>
+                        <span>Contrato</span>
+                        <p>PJ ou CLT</p>
+                    </li>
+                    <li>
+                        <i className="fa-solid fa-thumbs-up"></i>
+                        <span>100%</span>
+                        <p>Responsável</p>
+                    </li>
+                    <li>
+                        <i className="fa-regular fa-lightbulb"></i>
+                        <span>Perfil</span>
+                        <p>Criativo</p>
+                    </li>
+                    <li>
+                        <i className="fa-solid fa-microphone-lines"></i>
+                        <span>Boa</span>
+                        <p>Comunicação </p>
+                    </li>
+                    <li>
+                        <i className="fa-solid fa-heart-pulse"></i>
+                        <span>Realiza</span>
+                        <p>Atividade Física</p>
+                    </li>
+                </ul>
+            </div>
+
+            {/* <!-- <div className="funfacts">
+                                                                                                            <h2><i className="fa-solid fa-heart"></i>FATOS</h2>
+                                                                                                            <ul>
+                                                                                                                <li>
+                                                                                                                    <i className="fa-solid fa-mug-hot"></i>
+                                                                                                                    <span>100%</span>
+                                                                                                                    <p>Disponível</p>
+                                                                                                                </li>
+                                                                                                                <li>
+                                                                                                                    <i className="fa-solid fa-medal"></i>
+                                                                                                                    <span>32</span>
+                                                                                                                    <p>Cerificados</p>
+                                                                                                                </li>
+                                                                                                                <li>
+                                                                                                                    <i className="fa-solid fa-graduation-cap"></i>
+                                                                                                                    <span>Disposto</span>
+                                                                                                                    <p>Estudar </p>
+                                                                                                                </li>
+                                                                                                                <li>
+                                                                                                                    <i className="fa-sharp fa-solid fa-hand-holding-dollar"></i>
+                                                                                                                    <span>Contrato</span>
+                                                                                                                    <p>PJ ou CLT</p>
+                                                                                                                </li>
+                                                                                                            </ul>
+                                                                                                        </div> --> */}
+        </div>
+    )
+}
+
+export default Facts;
