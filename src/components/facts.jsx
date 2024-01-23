23bbe376f6620422629190918fd74f2543387e57
1andersonmotta <devandersonmotta@gmail.com>
2024-01-23 16:44:54 -0300

    fix adjust in icon import

--- src/components/facts.jsx
+++ src/components/facts.jsx
@@ -1,9 +1,23 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faGraduationCap, faHandHoldingDollar, faLightbulb, faMedal, faMugHot, faStar, faStarHalfStroke, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import {
+    faHandHoldingDollar,
+    faMugHot,
+    faMedal,
+    faGraduationCap,
+    faThumbsUp,
+    faLightbulb
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 // Adicione os ícones à biblioteca
-library.add(faHandHoldingDollar, faMugHot, faMedal, faGraduationCap, faThumbsUp, faLightbulb);
+library.add(
+    faHandHoldingDollar,
+    faMugHot,
+    faMedal,
+    faGraduationCap,
+    faThumbsUp,
+    faLightbulb
+);
 
 const Facts = () => {
     return (
@@ -12,32 +26,32 @@ const Facts = () => {
             <div className="funfacts">
                 <ul>
                     <li>
-                        <i><FontAwesomeIcon icon="fa-solid fa-hand-holding-dollar" /></i>
+                        <i><FontAwesomeIcon icon={faHandHoldingDollar} /></i>
                         <span>Contrato</span>
                         <p>PJ ou CLT</p>
                     </li>
                     <li>
-                        <i><FontAwesomeIcon icon="fa-solid fa-mug-hot" /></i>
+                        <i><FontAwesomeIcon icon={faMugHot} /></i>
                         <span>100%</span>
                         <p>Disponível</p>
                     </li>
                     <li>
-                        <i><FontAwesomeIcon icon="fa-solid fa-medal" /></i>
+                        <i><FontAwesomeIcon icon={faMedal} /></i>
                         <span>32</span>
                         <p>Cerificados</p>
                     </li>
                     <li>
-                        <i><FontAwesomeIcon icon="fa-solid fa-graduation-cap" /></i>
+                        <i><FontAwesomeIcon icon={faGraduationCap} /></i>
                         <span>Superior</span>
                         <p>ADS Cursando</p>
                     </li>
                     <li>
-                        <i><FontAwesomeIcon icon="fa-solid fa-thumbs-up" /></i>
+                        <i><FontAwesomeIcon icon={faThumbsUp} /></i>
                         <span>100%</span>
                         <p>Responsável</p>
                     </li>
                     <li>
-                        <i><FontAwesomeIcon icon="fa-solid fa-lightbulb" /></i>
+                        <i><FontAwesomeIcon icon={faLightbulb} /></i>
                         <span>Perfil</span>
                         <p>Criativo</p>
                     </li>
