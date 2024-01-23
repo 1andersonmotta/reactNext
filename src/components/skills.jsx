d9f8635b384e2f8b8306a4dec0deb7adea7d027c
1andersonmotta <devandersonmotta@gmail.com>
2024-01-23 16:45:05 -0300

    fix adjust in icon import

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,10 +1,10 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHandHoldingDollar, faStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons';
 import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 // Adicione os ícones à biblioteca
-library.add(faStar, faStarHalfStroke, farStar, faHandHoldingDollar);
+library.add(faStar, faStarHalfStroke, farStar);
 
 const Skills = () => {
     return (
@@ -14,194 +14,194 @@ const Skills = () => {
                 <div className="skill_content">
                     <h3>JavaScript </h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>TypeScript</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>NodeJS</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star-half-stroke" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStarHalfStroke} /></li>
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>PostgresSql</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star-half-stroke" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStarHalfStroke} /></li>
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>MySql</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" style={{ color: "#ffffff45;" }} /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>Sqlite</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>MongoDB</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star-half-stroke" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStarHalfStroke} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
 
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>Prisma</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star-half-stroke" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStarHalfStroke} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
 
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>TypeORM</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star-half-stroke" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStarHalfStroke} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>Sequelize</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star-half-stroke" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStarHalfStroke} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>Express</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>Docker</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
 
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>HTML</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>CSS</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>React</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>Serverless</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>Graphql</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>AWS</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
                     </ul>
                 </div>
                 <div className="skill_content">
                     <h3>Inglês</h3>
                     <ul>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-solid fa-star" /></li>
-                        <li><FontAwesomeIcon icon="fa-regular fa-star" /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={faStar} /></li>
+                        <li><FontAwesomeIcon icon={farStar} /></li>
 
                     </ul>
                 </div>
