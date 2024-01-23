561019eb1d9de240a476e8ba06fd1cbf4c4566df
1andersonmotta <devandersonmotta@gmail.com>
2024-01-23 13:45:03 -0300

    feature add import in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import type { AppProps } from 'next/app'
 import '@/styles/globals.css'
 import '@/styles/style.css'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGraduationCap, faHandHoldingDollar, faLightbulb, faMedal, faMugHot, faStar, faStarHalfStroke, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// Adicione os ícones à biblioteca
+library.add(faHandHoldingDollar, faMugHot, faMedal, faGraduationCap, faThumbsUp, faLightbulb);
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
