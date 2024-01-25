4a888b6ee40ac6ecd645dddea37be2014a222ee7
1andersonmotta <devandersonmotta@gmail.com>
2024-01-25 18:21:40 -0300

    adjust size for icons contact

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -13,11 +13,11 @@ const FooterComponent = () => {
             </div>
             <nav aria-label="navegação de links sociais">
                 <ul className="footer_sociais">
-                    <li><a href="https://www.linkedin.com/in/anderson-motta-96b138235/" target="_blank"><i><CiLinkedin size={80} /></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/anderson-motta-96b138235/" target="_blank"><i><CiLinkedin size={84} /></i></a></li>
                     <li><a href="https://wa.me/5515997897412" target="_blank"><i><FaWhatsapp size={80} /></i></a></li>
-                    <li><a href="https://github.com/1andersonmotta" target="_blank"><i><FaGithub size={80} /></i></a></li>
+                    <li><a href="https://github.com/1andersonmotta" target="_blank"><i><FaGithub size={78} /></i></a></li>
                     <li><a href="https://www.instagram.com/djanderson_motta/" target="_blank"><i><FaInstagram size={80} /></i></a></li>
-                    <li><a href="https://www.facebook.com/djandersonmotta" target="_blank"><i><FaFacebook size={80} /></i></a></li>
+                    <li><a href="https://www.facebook.com/djandersonmotta" target="_blank"><i><FaFacebook size={78} /></i></a></li>
                 </ul>
             </nav>
             <p className="footer_comercial">Todos os direitos reservados - DEV ANDERSON MOTTA &copy; 2023</p>
