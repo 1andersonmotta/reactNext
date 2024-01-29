7ee73db38e75e7bcadac7caad33315dcae0fcb05
1andersonmotta <devandersonmotta@gmail.com>
2024-01-29 17:41:08 -0300

    fix add types

--- src/components/component.tsx
+++ src/components/component.tsx
@@ -4,15 +4,15 @@ const Component = () => {
     const tituloRef = useRef(null);
 
     useEffect(() => {
-        function ativaLetra(elemento) {
+        function ativaLetra(elemento: any) {
             const arrTexto = elemento.innerHTML.split('');
             elemento.innerHTML = '';
-            arrTexto.forEach((letra, i) => {
+            arrTexto.forEach((letra: any, i: number) => {
                 setTimeout(() => {
                     elemento.innerHTML += letra;
                 }, 75 * i);
             });
-        }
+        };
 
         const titulo = tituloRef.current;
         ativaLetra(titulo);
@@ -21,9 +21,8 @@ const Component = () => {
             ativaLetra(titulo);
         }, 5000);
 
-        // Limpando o intervalo quando o componente é desmontado
         return () => clearInterval(intervalId);
-    }, []); // O array de dependências vazio garante que o useEffect só será executado uma vez
+    }, []);
 
     return (
         <section aria-label="seção curriculo download Anderson" className="section-div">
